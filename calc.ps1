0db9abf8728fdef4b29ee911e9c9accfdf570b13
Swishers223 <41699111+Swishers223@users.noreply.github.com>
2021-04-25 10:28:27 +0300

    Update calc.ps1

--- calc.ps1
+++ calc.ps1
@@ -1,164 +1,85 @@
-[int]$var1
-[int]$var2
-[string]$var3
-[int]$var4
+#Global variables
+[string]$global:yes = "y"
+[string]$global:no = "n"
+[string]$global:yesorno
+[int]$global:solution
+
+#Functions
+function Get-MathOperations {
+    [CmdletBinding()]
+    param (
+        [string]
+        $operation,
+
+        [int]
+        $int1,
+
+        [int]
+        $int2
+    )
+            
+    #Number 1
+    $int1 = Read-Host "Number 1"
+
+    #Number 2
+    $int2 = Read-Host "Number 2"
+
+    #Operations
+        $operation = Read-Host "What operation do you need? ( + | - | * | / )"
+        if ($operation -eq "+") {
+            $global:solution = $int1 + $int2
+        }
+        elseif ($operation -eq "-") {
+            $global:solution = $int1 - $int2
+        }
+        elseif ($operation -eq "*") {
+            $global:solution = $int1 * $int2
+        }
+        elseif ($operation -eq "/") {
+            $global:solution = $int1 / $int2
+        }
+}
+
+function Get-YesOrNo {
+    Write-Output "Do you wanna use my calc?"
+    $global:yesorno = Read-Host "( Y | N )"
+}
 
 Clear-Host
-$language = Read-Host -Prompt 'Choose your language/Выберите свой язык ( ENG | РУС )'
-Clear-Host
+Get-YesOrNo
 
-$language
-
-if ($language -like "eng") {
-$Message = 'Do you wanna use my calc? ( Y | N )'
-$yes = "y"
-$no = "n"
-Clear-Host
-
-Write-Output $Message
-$answer = Read-Host
-Clear-Host
-if ($answer -contains $yes) {
-    while ($answer -contains $yes) {
-        Write-Output "Write down some numbers!"
-        [int]$var1 = Read-Host -Prompt "Number 1"
-        [int]$var2 = Read-Host -Prompt "Number 2"
-        [string]$var3 = Read-Host -Prompt "What operation do you need? ( + | - | * | / )"
-        if ($var3 -eq "+") {
-            $var4 = $var1 + $var2
-        }
-        if ($var3 -eq "-") {
-            $var4 = $var1 - $var2
-        }
-        if ($var3 -eq "*") {
-            $var4 = $var1 * $var2
-        }
-        if ($var3 -eq "/") {
-            $var4 = $var1 / $var2
-        }
-        Write-Output "Anwser: $var4"
+if ($global:yesorno -contains $global:yes) {
+    while ($global:yesorno -contains $global:yes) {
+        Get-MathOperations
+        Write-Output "Solution: $solution"
         Start-Sleep -Seconds 3
         Clear-Host
-        Clear-Variable -Name var1
-        Clear-Variable -Name var2
-        Clear-Variable -Name var3
-        Clear-Variable -Name var4
-        Clear-Variable -Name answer
-        Write-Output $Message
-        $answer = Read-Host
-        Clear-Host
-            if ($answer -contains $no) {
+        Get-YesOrNo
+            if ($global:yesorno -contains $global:no) {
                 Write-Output "( ˘_˘ )"
                 Start-Sleep -Seconds 3
+                Clear-Host
                 break
             }
-            ElseIf ($answer -ne $no -ne $yes) {
-                Clear-Host
+            ElseIf ($global:yesorno -ne $global:no -ne $global:yes) {
                 Write-Output "Please, use <Y> or <N>!"
                 Start-Sleep -Seconds 3
-                $answer = $yes
                 Clear-Host
+                break
             }
     }
 }
-ElseIf ($answer -contains $no) {
-    Clear-Host
+
+ElseIf ($global:yesorno -contains $global:no) {
     Write-Output "( ˘_˘ )"
     Start-Sleep -Seconds 3
     Clear-Host
     break
 }
+
 Else {
-    Clear-Host
     Write-Output "Please, use <Y> or <N>!"
     Start-Sleep -Seconds 3
     Clear-Host
     break
-}
-
-
-Clear-Variable -Name var1
-Clear-Variable -Name var2
-Clear-Variable -Name var3
-Clear-Variable -Name var4
-Clear-Variable -Name answer
-Clear-Host
-break
-}
-
-ElseIf ($language -contains "рус") {
-$Message = "Хотите ли вы опробовать мой новый калькулятор? ( Д | Н )"
-$yes = "д"
-$no = "н"
-Clear-Host
-
-Write-Output $Message
-$answer = Read-Host
-Clear-Host
-if ($answer -contains $yes) {
-    while ($answer -contains $yes) {
-            Write-Output "Напишите пару чисел!"
-            [int]$var1 = Read-Host -Prompt "Число 1"
-            [int]$var2 = Read-Host -Prompt "Число 2"
-            [string]$var3 = Read-Host -Prompt "Какую операцию будем над ними выполнять? ( + | - | * | / )"
-            if ($var3 -eq "+") {
-                $var4 = $var1 + $var2
-            }
-            if ($var3 -eq "-") {
-                $var4 = $var1 - $var2
-            }
-            if ($var3 -eq "*") {
-                $var4 = $var1 * $var2
-            }
-            if ($var3 -eq "/") {
-                $var4 = $var1 / $var2
-            }
-            Write-Output "Ответ: $var4"
-            Start-Sleep -Seconds 3
-            Clear-Host
-            Clear-Variable -Name var1
-            Clear-Variable -Name var2
-            Clear-Variable -Name var3
-            Clear-Variable -Name var4
-            Clear-Variable -Name answer
-            Write-Output $Message
-            $answer = Read-Host
-            Clear-Host
-                if ($answer -contains $no) {
-                    Write-Output "( ˘_˘ )"
-                    Start-Sleep -Seconds 3
-                    break
-                }
-                ElseIf ($answer -ne $no -ne $yes) {
-                    Clear-Host
-                    Write-Output "Пожалуйста, используйте <Д> или <Н>!"
-                    Start-Sleep -Seconds 3
-                    $answer = $yes
-                    Clear-Host
-                }
-    }
-}
-
-ElseIf ($answer -contains $no) {
-    Clear-Host
-    Write-Output "( ˘_˘ )"
-    Start-Sleep -Seconds 3
-    Clear-Host
-    break
-}
-Else {
-    Clear-Host
-    Write-Output "Пожалуйста, используйте <д> или <н>!"
-    Start-Sleep -Seconds 3
-    Clear-Host
-    break
-}
-
-Clear-Variable -Name var1
-Clear-Variable -Name var2
-Clear-Variable -Name var3
-Clear-Variable -Name var4
-Clear-Variable -Name answer
-Clear-Host
-break
 }
